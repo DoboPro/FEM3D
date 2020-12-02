8db280800322a749f136df2567dce5312d64b21d
sasaco <sasaicco@gmail.com>
2020-12-02 19:41:54 +0900

    initial

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // 計算を開始する
+  public calcStart() {
+  }
+
+
+
+}
