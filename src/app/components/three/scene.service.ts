7f8f194197a87b357399012d8b857a4bc0a500f1
sasaco <sasaicco@gmail.com>
2021-01-29 22:04:36 +0900

    解析できた

---
+++ src/app/components/three/scene.service.ts
@@ -0,0 +1,192 @@
+import { Injectable } from '@angular/core';
+import * as THREE from 'three';
+import { GUI } from './libs/dat.gui.module.js';
+import { OrbitControls } from './libs/OrbitControls.js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SceneService {
+
+  // シーン
+  private scene: THREE.Scene;
+
+  // レンダラー
+  private renderer: THREE.WebGLRenderer;
+
+  // カメラ
+  private camera: THREE.PerspectiveCamera;
+
+  // helper
+  private axisHelper: THREE.AxesHelper;
+  private GridHelper: THREE.GridHelper;
+
+  // gui
+  public gui: GUI;
+  private params: any;          // GridHelperの表示制御
+  cone1: any;
+  cone2: any;
+
+  // 初期化
+  public constructor() {
+    // シーンを作成
+    this.scene = new THREE.Scene();
+    // シーンの背景を白に設定
+    this.scene.background = new THREE.Color(0xf0f0f0);
+    // レンダラーをバインド
+    this.render = this.render.bind(this);
+
+    // gui
+    this.params = {
+      GridHelper: true
+    };
+  }
+
+  public OnInit(aspectRatio: number,
+    canvasElement: HTMLCanvasElement,
+    deviceRatio: number,
+    Width: number,
+    Height: number): void {
+    // カメラ
+    this.createCamera(aspectRatio);
+    // 環境光源
+    this.add(new THREE.AmbientLight(0xf0f0f0));
+    // レンダラー
+    this.createRender(canvasElement,
+      deviceRatio,
+      Width,
+      Height);
+    // コントロール
+    this.addControls();
+
+    // 床面を生成する
+    this.createHelper();
+
+    //
+    this.gui = new GUI();
+    this.gui.domElement.id = 'gui_css';
+    this.gui.add(this.params, 'GridHelper').onChange((value) => {
+      // guiによる設定
+      this.GridHelper.visible = value;
+      this.render();
+    });
+    this.gui.open();
+  }
+
+  // 床面を生成する
+  private createHelper() {
+    this.axisHelper = new THREE.AxesHelper(200);
+    this.scene.add(this.axisHelper);
+    this.axisHelper.visible = false;
+
+    this.GridHelper = new THREE.GridHelper(200, 20);
+    this.GridHelper.geometry.rotateX(Math.PI / 2);
+    this.GridHelper.material['opacity'] = 0.2;
+    this.GridHelper.material['transparent'] = true;
+    this.scene.add(this.GridHelper);
+
+  }
+
+  // コントロール
+  public addControls() {
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.damping = 0.2;
+    controls.addEventListener('change', this.render);
+  }
+
+  // 物体とマウスの交差判定に用いるレイキャスト
+  public getRaycaster(mouse: THREE.Vector2): THREE.Raycaster {
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, this.camera);
+    return raycaster;
+  }
+
+  // カメラの初期化
+  public createCamera(aspectRatio: number) {
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      aspectRatio,
+      0.1,
+      1000
+    );
+    this.camera.position.set(0, -50, 20);
+    this.scene.add(this.camera);
+  }
+
+  // レンダラーを初期化する
+  public createRender(canvasElement: HTMLCanvasElement,
+    deviceRatio: number,
+    Width: number,
+    Height: number): void {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: canvasElement,
+      alpha: true,    // transparent background
+      antialias: true // smooth edges
+    });
+    this.renderer.setPixelRatio(deviceRatio);
+    this.renderer.setSize(Width, Height);
+    this.renderer.shadowMap.enabled = true;
+  }
+
+  public RendererDomElement(): Node {
+    return this.renderer.domElement;
+  }
+
+
+  // リサイズ
+  public onResize(deviceRatio: number,
+    Width: number,
+    Height: number): void {
+    this.camera.aspect = deviceRatio;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(Width, Height);
+    this.render();
+  }
+
+  // レンダリングする
+  public render() {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  // レンダリングのサイズを取得する
+  public getBoundingClientRect(): ClientRect | DOMRect {
+    return this.renderer.domElement.getBoundingClientRect();
+  }
+
+  // シーンにオブジェクトを追加する
+  public add(...threeObject: THREE.Object3D[]): void {
+    for (const obj of threeObject) {
+      this.scene.add(obj);
+    }
+  }
+
+  // シーンのオブジェクトを削除する
+  public remove(...threeObject: THREE.Object3D[]): void {
+    for (const obj of threeObject) {
+      this.scene.remove(obj);
+    }
+  }
+
+  // シーンにオブジェクトを削除する
+  public removeByName(...threeName: string[]): void {
+    for (const name of threeName) {
+      const target = this.scene.getObjectByName(name);
+      if (target === undefined) {
+        continue;
+      }
+      this.scene.remove(target);
+    }
+  }
+
+  // ファイルに視点を保存する
+  public getSettingJson(): any {
+    return {
+      camera: {
+        x: this.camera.position.x,
+        y: this.camera.position.y,
+        z: this.camera.position.z,
+      }
+    };
+  }
+
+}
---
+++ src/app/components/three/three.component.ts
@@ -0,0 +1,99 @@
+import { AfterViewInit, Component, ElementRef, ViewChild, HostListener, NgZone } from '@angular/core';
+import * as THREE from 'three';
+
+import { SceneService } from './scene.service';
+import { ThreeService } from './three.service';
+
+
+@Component({
+  selector: 'app-three',
+  templateUrl: './three.component.html',
+  styleUrls: ['./three.component.scss']
+})
+export class ThreeComponent implements AfterViewInit {
+  
+  @ViewChild('myCanvas', { static: true }) private canvasRef: ElementRef;
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+  constructor(private ngZone: NgZone,
+              private scene: SceneService,
+              private three: ThreeService) { 
+    THREE.Object3D.DefaultUp.set(0, 0, 1);
+  }
+
+  ngAfterViewInit() {
+    this.scene.OnInit(this.getAspectRatio(),
+      this.canvas,
+      devicePixelRatio,
+      window.innerWidth,
+      window.innerHeight);
+
+    // ラベルを表示する用のレンダラーを HTML に配置する
+    const element = this.scene.RendererDomElement();
+    const div = document.getElementById('myCanvas');        // ボタンを置きたい場所の手前の要素を取得
+    div.parentNode.insertBefore(element, div.nextSibling);  // ボタンを置きたい場所にaタグを追加
+
+    // レンダリングする
+    this.animate();
+  }
+
+  animate(): void {
+    // We have to run this outside angular zones,
+    // because it could trigger heavy changeDetection cycles.
+    this.ngZone.runOutsideAngular(() => {
+      window.addEventListener('DOMContentLoaded', () => {
+        this.scene.render();
+      });
+    });
+  }
+
+  // マウスクリック時のイベント
+  @HostListener('mousedown', ['$event'])
+  public onMouseDown(event: MouseEvent) {
+    const mouse: THREE.Vector2 = this.getMousePosition(event);
+    this.three.detectObject(mouse, 'click');
+  }
+
+  // マウスクリック時のイベント
+  @HostListener('mouseup', ['$event'])
+  public onMouseUp(event: MouseEvent) {
+    const mouse: THREE.Vector2 = this.getMousePosition(event);
+    this.three.detectObject(mouse, 'select');
+  }
+
+  // マウス移動時のイベント
+  @HostListener('mousemove', ['$event'])
+  public onMouseMove(event: MouseEvent) {
+    const mouse: THREE.Vector2 = this.getMousePosition(event);
+    this.three.detectObject(mouse, 'hover');
+  }
+
+  // マウス位置とぶつかったオブジェクトを検出する
+  private getMousePosition(event: MouseEvent): THREE.Vector2 {
+    event.preventDefault();
+    const rect = this.scene.getBoundingClientRect();
+    const mouse = new THREE.Vector2();
+    mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    mouse.y = - ((event.clientY - rect.top) / rect.height) * 2 + 1;
+    return mouse;
+  }
+
+  // ウインドウがリサイズした時のイベント処理
+  @HostListener('window:resize', ['$event'])
+  public onResize(event: Event) {
+    this.scene.onResize(this.getAspectRatio(),
+      window.innerWidth,
+      window.innerHeight - 120);
+  }
+
+  private getAspectRatio(): number {
+    if (this.canvas.clientHeight === 0) {
+      return 0;
+    }
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
+
+}
