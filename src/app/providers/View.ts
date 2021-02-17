0e65ab32b7897270b7dadc5e28c5b73814c06fbf
mizutani488 <g1918082@tcu.ac.jp>
2021-02-17 18:47:57 +0900

    たわんだ！！

--- src/app/providers/View.ts
+++ src/app/providers/View.ts
@@ -4,70 +4,75 @@ import { BoundaryCondition } from './boundary/BoundaryCondition';
 import { FemDataModel } from './FemDataModel';
 import { Result } from './Result';
 import { MeshModel } from './mesh/MeshModel';
-
+import { SceneService } from '../components/three/scene.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 // 連立方程式求解オブジェクト
 export class View {
-  
+  public PRECISION = 1e-10; // マトリックス精度
+  public LU_METHOD = 0; // LU分解法
+  public ILUCG_METHOD = 1; // 不完全LU分解共役勾配法
 
-    public PRECISION = 1e-10;	// マトリックス精度
-    public LU_METHOD = 0;	    // LU分解法
-    public ILUCG_METHOD = 1;	// 不完全LU分解共役勾配法
-  
-    public matrix: any[];		// 行列
-    public matrix2: any[];  // 第２行列
-    public vector: any[];		// ベクトル
-    public dof: number;			// モデル自由度
-    public method: number;	// 方程式解法
-  
-  
-    constructor(private model: FemDataModel,
-                 private mesh: MeshModel,
-                private result: Result) {
-      
-    }
-  
-    
-    public setDisplacement(disp,coef){
-      if(disp.length===0) return;
-      this.setGeomDisplacement1(this.mesh.geometry_mesh,disp,coef);
-      this.setGeomDisplacement2(this.mesh.geometry_edge,disp,coef);
-    };
-  
-    public setGeomDisplacement1(geometry_mesh,disp,coef){
-      var label=geometry_mesh.nodes,
-      nodes=this.model.mesh.nodes,angle=geometry_mesh.angle;
-      var pos=geometry_mesh.attributes.position.array;
-      for(var i=0;i<geometry_mesh.length;i++){
-        var i3=3*i,p=nodes[label[i]],dx=disp[label[i]].x;
-        pos[i3]=p.x+  coef*dx[0];
-        pos[i3+1]=p.y+coef*dx[1];
-        pos[i3+2]=p.z+coef*dx[2];
-        angle[i3]=coef*dx[3];
-        angle[i3+1]=coef*dx[4];
-        angle[i3+2]=coef*dx[5];
-      }
-      geometry_mesh.attributes.position.needsUpdate=true;
-    }
-  
-    public setGeomDisplacement2(geometry_edge,disp,coef){
-      var label=geometry_edge.nodes,
-      nodes=this.model.mesh.nodes,angle=geometry_edge.angle;
-      var pos=geometry_edge.attributes.position.array;
-      for(var i=0;i<label.length;i++){
-        var i3=3*i,p=nodes[label[i]],dx=disp[label[i]].x;
-        pos[i3]=p.x+  coef*dx[0];
-        pos[i3+1]=p.y+coef*dx[1];
-        pos[i3+2]=p.z+coef*dx[2];
-        angle[i3]=coef*dx[3];
-        angle[i3+1]=coef*dx[4];
-        angle[i3+2]=coef*dx[5];
-      }
-      geometry_edge.attributes.position.needsUpdate=true;
-    }
-  
+  public matrix: any[]; // 行列
+  public matrix2: any[]; // 第２行列
+  public vector: any[]; // ベクトル
+  public dof: number; // モデル自由度
+  public method: number; // 方程式解法
+
+  constructor(
+    private model: FemDataModel,
+    private mesh: MeshModel,
+    private scene: SceneService,
+    private result: Result
+  ) {}
+
+  public setDisplacement(disp) {
+    if (disp.length === 0) return;
+    this.setGeomDisplacement1(this.mesh.geometry_mesh, disp);
+    this.setGeomDisplacement2(this.mesh.geometry_edge, disp);
   }
+
+  public setGeomDisplacement1(geometry_mesh, disp) {
+    const coef = 0.1;
+    var label = geometry_mesh.nodes,
+      nodes = this.model.mesh.nodes,
+      angle = geometry_mesh.angle;
+    var pos = geometry_mesh.attributes.position.array;
+    for (var i = 0; i < label.length; i++) {
+      var i3 = 3 * i,
+        p = nodes[label[i]],
+        dx = disp[label[i]].x;
+        console.log(pos[i3])
+      pos[i3] = p.x + coef * dx[0];
+      pos[i3 + 1] = p.y + coef * dx[1];
+      pos[i3 + 2] = p.z + coef * dx[2];
+      angle[i3] = coef * dx[3];
+      angle[i3 + 1] = coef * dx[4];
+      angle[i3 + 2] = coef * dx[5];
+    }
+    geometry_mesh.attributes.position.needsUpdate = true;
+  }
+
+  public setGeomDisplacement2(geometry_edge, disp) {
+    const coef = 0.1;
+    var label = geometry_edge.nodes,
+      nodes = this.model.mesh.nodes,
+      angle = geometry_edge.angle;
+    var pos = geometry_edge.attributes.position.array;
+    for (var i = 0; i < label.length; i++) {
+      var i3 = 3 * i,
+        p = nodes[label[i]],
+        dx = disp[label[i]].x;
+      pos[i3] = p.x + coef * dx[0];
+      pos[i3 + 1] = p.y + coef * dx[1];
+      pos[i3 + 2] = p.z + coef * dx[2];
+      angle[i3] = coef * dx[3];
+      angle[i3 + 1] = coef * dx[4];
+      angle[i3 + 2] = coef * dx[5];
+    }
+    geometry_edge.attributes.position.needsUpdate = true;
+  }
+}
