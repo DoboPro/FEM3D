4f5e22cfaa2cce0c03bf5374c1129aaf03d950bf
mizutani488 <g1918082@tcu.ac.jp>
2021-03-05 16:11:50 +0900

    反復解法の機能をいれた。 地盤データを追加。

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FemMainService } from 'src/app/providers/FemMain';
+import { Result } from 'src/app/providers/Result';
 import { Solver } from 'src/app/providers/Solver';
 import { ThreeService } from '../three/three.service';
 
@@ -9,7 +10,14 @@ import { ThreeService } from '../three/three.service';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
-  constructor(public InputData: FemMainService, public Solver: Solver) {}
+  public myElement = document.getElementById('result');
+  public d: string;
+
+  constructor(
+    public InputData: FemMainService,
+    public result: Result,
+    public Solver: Solver
+  ) {}
 
   ngOnInit(): void {
     this.renew();
@@ -17,12 +25,28 @@ export class MenuComponent implements OnInit {
 
   // 計算
   public calcrate() {
+    //計算開始
     this.Solver.calcStart();
+
+    //計算後、menuバー上に変位量の最大値を表示させる
+    const w = this.result.dispMax.toFixed(3);
+    this.d = w;
+    // const w:number = this.d.toFixed(3);
+    const elem = document.getElementById('result').style;
+    const myStyle = {
+      display: 'block',
+    };
+    for (const prop in myStyle) {
+      elem[prop] = myStyle[prop];
+    }
+
+    //コンター図を出す
     this.Solver.conterStart();
   }
 
   // 新規作成
   public renew(): void {
-    this.InputData.initModel('assets/beam/sampleBeamHexa1.fem');
+    this.InputData.initModel('assets/ground/groundsimpleHexa2.fem');
+    //this.InputData.initModel('assets/bend/sampleBendHexa1.fem');
   }
 }
