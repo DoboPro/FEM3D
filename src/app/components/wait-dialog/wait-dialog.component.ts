3846c6dde0e45f86d742df8173b9a1a021454804
mizutani488 <g1918082@tcu.ac.jp>
2021-03-06 18:09:51 +0900

    modalつけた

--- src/app/components/wait-dialog/wait-dialog.component.ts
+++ src/app/components/wait-dialog/wait-dialog.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Solver } from 'src/app/providers/Solver';
+// import { Result } from 'src/app/providers/Result';
 
 @Component({
   selector: 'app-wait-dialog',
   templateUrl: './wait-dialog.component.html',
-  styleUrls: ['./wait-dialog.component.scss']
+  styleUrls: ['./wait-dialog.component.scss'],
 })
-export class WaitDialogComponent implements OnInit {
+export class WaitDialogComponent implements AfterViewInit {
+  constructor(
+    public activeModal: NgbActiveModal,
+    public Solver: Solver,
+    // public result: Result
+  ) {}
 
-  constructor() { }
+  ngAfterViewInit() {
+    console.log("sasa-WaitDialogComponent ngAfterContentInit");
 
-  ngOnInit() {
+    setTimeout(()=>{
+      const solve = this.Solver.calcStart();
+      console.log('sasa- calcrate end');
+      this.Solver.conterStart();
+      console.log('sasa- activeModal.conterStart');
+      this.activeModal.close('e');
+      console.log('sasa- activeModal.close');
+    },1);
+
+
+    // console.log(solve);
+    // //計算後、menuバー上に変位量の最大値を表示させる
+    // const w = this.result.dispMax.toFixed(3);
+    // const d = w;
+    // //コンター図を出す
+
+
+    
   }
-
+  // 計算
+  // public calcrate() {
+  //   //計算開始
+  //   const solve = this.Solver.calcStart();
+  //   console.log(solve);
+  //   //計算後、menuバー上に変位量の最大値を表示させる
+  //   const w = this.result.dispMax.toFixed(3);
+  //   const d = w;
+  //   //コンター図を出す
+  //   this.Solver.conterStart();
+  // }
 }
