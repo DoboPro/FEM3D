2de4be226009d2663291fd9e84d74ae6989f22e3
sasaco <sasaicco@gmail.com>
2021-03-26 12:05:22 +0900

    多少スリム化した

--- src/app/providers/border/ElementBorder.ts
+++ src/app/providers/border/ElementBorder.ts
@@ -9,20 +9,6 @@ import { Injectable } from '@angular/core';
 // nodes - 節点番号
 // intP - 積分点のξ,η座標,重み係数
 export class ElementBorder {
-
-  /*
-  // 四角形2次要素の積分点のξ,η座標,重み係数
-var QUAD2_INT = [[GX3[0], GX3[0], GW3[0] * GW3[0]],
-[GX3[1], GX3[0], GW3[1] * GW3[0]],
-[GX3[2], GX3[0], GW3[2] * GW3[0]],
-[GX3[0], GX3[1], GW3[0] * GW3[1]],
-[GX3[1], GX3[1], GW3[1] * GW3[1]],
-[GX3[2], GX3[1], GW3[2] * GW3[1]],
-[GX3[0], GX3[2], GW3[0] * GW3[2]],
-[GX3[1], GX3[2], GW3[1] * GW3[2]],
-  [GX3[2], GX3[2], GW3[2] * GW3[2]]];
-*/
-
   public element: number;
   public intP: number[][];
   public isEdge: boolean;		// 辺ではない
@@ -54,88 +40,4 @@ var QUAD2_INT = [[GX3[0], GX3[0], GW3[0] * GW3[0]],
     }
     return p1.length - p2.length;
   }
-
-  /*
-  // 要素境界のコピーを返す
-  public clone() {
-    return new ElementBorder(this.element, this.nodes.concat());
-  };
-
-
-  // 擬似ヤコビアンを返す
-  // p - 節点
-  // sf - 形状関数行列
-  public jacobian(p, sf) {
-    var count = this.nodeCount(), jac = [0, 0, 0, 0, 0, 0];
-    for (var i = 0; i < count; i++) {
-      for (var j = 0; j < 2; j++) {
-        jac[j] += sf[i][j + 1] * p[i].x;
-        jac[j + 2] += sf[i][j + 1] * p[i].y;
-        jac[j + 4] += sf[i][j + 1] * p[i].z;
-      }
-    }
-    var j1 = jac[2] * jac[5] - jac[4] * jac[3];
-    var j2 = jac[4] * jac[1] - jac[0] * jac[5];
-    var j3 = jac[0] * jac[3] - jac[2] * jac[1];
-    return Math.sqrt(j1 * j1 + j2 * j2 + j3 * j3);
-  };
-
-  // 積分点の形状関数マトリックス [ NiNj ] を返す
-  // p - 節点
-  // x - ξ,η座標
-  // w - 重み係数
-  public shapeMatrixPart(p, x, w) {
-    var sf = this.shapeFunction(x[0], x[1]);
-    var coef = w * this.jacobian(p, sf);
-    var count = this.nodeCount(), matrix = [];
-    for (var i = 0; i < count; i++) {
-      matrix[i] = [];
-      var cf2 = coef * sf[i][0];
-      for (var j = 0; j < count; j++) {
-        matrix[i][j] = cf2 * sf[j][0];
-      }
-    }
-    return matrix;
-  };
-
-  // 積分点の形状関数ベクトル [ Ni ] を返す
-  // p - 節点
-  // x - ξ,η座標
-  // w - 重み係数
-  public shapeVectorPart(p, x, w) {
-    var sf = this.shapeFunction(x[0], x[1]);
-    var coef = w * this.jacobian(p, sf);
-    var count = this.nodeCount(), vector = [];
-    for (var i = 0; i < count; i++) {
-      vector[i] = coef * sf[i][0];
-    }
-    return vector;
-  };
-
-  // 形状関数マトリックス [ ∫NiNjdS ] を返す
-  // p - 節点
-  // coef - 係数
-  public shapeFunctionMatrix(p, coef) {
-    var count = this.nodeCount(), s = numeric.rep([count, count], 0);
-    for (var i = 0; i < this.intP.length; i++) {
-      addMatrix(s, this.shapeMatrixPart(p, this.intP[i],
-        coef * this.intP[i][2]));
-    }
-    return s;
-  };
-
-  // 形状関数ベクトル [ ∫NidS ] を返す
-  // p - 節点
-  // coef - 係数
-  public shapeFunctionVector(p, coef) {
-    var count = this.nodeCount(), s = numeric.rep([count], 0);
-    for (var i = 0; i < this.intP.length; i++) {
-      addVector(s, this.shapeVectorPart(p, this.intP[i],
-        coef * this.intP[i][2]));
-    }
-    return s;
-  }
-
-  */
-
 }
--- src/app/providers/elements/FElement.ts
+++ src/app/providers/elements/FElement.ts
@@ -68,39 +68,11 @@ export class FElement extends Comon {
   // p - 頂点座標
   // axis - 断面基準方向ベクトル
   public dirVectors(p: any[], axis): any[] {
-    // let v1, v2, v3;
-    /*
-    if (p.length == 2) {		// 梁要素
-      v1 = p[1].clone().sub(p[0]).normalize();
-      v2 = new THREE.Vector3();
-      v3 = new THREE.Vector3();
-      if ((axis !== null) && (axis !== undefined)) {
-        const dt = v1.dot(axis);
-        v2.set(axis.x - dt * v1.x, axis.y - dt * v1.y, axis.z - dt * v1.z);
-        if (v2.lengthSq() > 0) v2.normalize();
-      }
-      if (v2.lengthSq() === 0) {
-        if (Math.abs(v1.x) < Math.abs(v1.y)) {
-          v2.set(1 - v1.x * v1.x, -v1.x * v1.y, -v1.x * v1.z).normalize();
-        }
-        else {
-          v2.set(-v1.y * v1.x, 1 - v1.y * v1.y, -v1.y * v1.z).normalize();
-        }
-      }
-      v3.crossVectors(v1, v2);
-      return [v1, v2, v3];
-    }
-    else if (p.length > 2) {		// シェル要素
-    */
     const v3: THREE.Vector3 = this.normalVector(p);
     let  v2 = p[1].clone().sub(p[0]);
     v2 = v3.clone().cross(v2).normalize();
     const v1 = v2.clone().cross(v3);
     return [v1, v2, v3];
-    /*
-    }
-    return null;
-    */
   }
 
 
@@ -225,49 +197,4 @@ export class FElement extends Comon {
     const a3 = max(min(-v31.dot(v23), 1), -1);
     return acos(a1) + acos(a2) + acos(a3) - Math.PI;
   }
-  
-  
-  /*
-  // 要素境界を返す
-  // element - 要素ラベル
-  public borders(element) {
-    const count = this.borderCount(), borders = [];
-    for (let i = 0; i < count; i++) borders[i] = this.border(element, i);
-    return borders;
-  }
-
-
-
-  // 節点変位を局所座標系・1次元配列に変換する
-  // u - 節点変位
-  // d - 方向余弦マトリックス
-  public toLocalArray(u, d) {
-    const v = [], j;
-    for (let i = 0; i < 2; i++) {
-      const ux = u[i].x;
-      for (j = 0; j < 3; j++) {
-        v.push(d[0][j] * ux[0] + d[1][j] * ux[1] + d[2][j] * ux[2]);
-      }
-      for (j = 0; j < 3; j++) {
-        v.push(d[0][j] * ux[3] + d[1][j] * ux[4] + d[2][j] * ux[5]);
-      }
-    }
-    return v;
-  }
-
-  // 節点を入れ替える
-  // i1,i2 - 節点インデックス
-  public swap(nodes, i1, i2) {
-    const t = nodes[i1];
-    nodes[i1] = nodes[i2];
-    nodes[i2] = t;
-  }
-
-
-
-
-
-
-  */
-
 }
