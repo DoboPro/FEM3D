be2854487f9c75b3a2ca7b717385f5a2ff9a7b3c
mizutani488 <g1918082@tcu.ac.jp>
2021-09-25 11:46:30 +0900

    サンプルデータを追加

--- src/app/components/fileIndex/file-index.service.ts
+++ src/app/components/fileIndex/file-index.service.ts
@@ -7,25 +7,24 @@ export class FileIndexService {
   public selectedIndex = '1';
 
   public FEMlist: any[] = [
-    { id: '1', 
-      name: '片持ち梁', 
-      file: 'assets/beam/beamHexa_mesh_l.fem' 
-    },
+    { id: '1', name: '片持ち梁', file: 'assets/beam/beamHexa_mesh_l.fem' },
     {
       id: '2',
       name: '片持ち梁(分割数多め)',
       file: 'assets/beam/beamHexa_mesh_m.fem',
     },
-    { id: '3',
-      name: '曲がり梁', 
-      file: 'assets/bend/bendHexa.fem' 
-    },
+    { id: '3', name: '曲がり梁', file: 'assets/bend/bendHexa.fem' },
     {
       id: '4',
       name: '地盤(拘束条件のみ考慮)',
       file: 'assets/ground/groundHexa.fem',
     },
+    {
+      id: '5',
+      name: 'フィンガースピナー',
+      file: 'assets/finger/finger.fem',
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 }
